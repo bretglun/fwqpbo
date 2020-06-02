7c793b3248e93213584cce0ad298c1f18b0713c4
johan <johan.berglund@neuroradkarolinska.se>
2020-06-02 21:29:46 +0200

    demo.py: shebang and excecutable permission

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import FWQPBO
 import numpy as np
 import dicom
@@ -31,36 +33,39 @@ def getScore(case, dir):
         (1 - np.sum((np.abs(refFF - recFF) > 0.1) * mask) / np.sum(mask))
     return score
 
-if not os.path.isfile(r'./challenge/refdata.mat'):
-    url = r'"https://dl.dropboxusercontent.com/u/5131732/' + \
-        r'ISMRM_Challenge/refdata.mat"'
-    raise Exception(r'Please download ISMRM 2012 challenge reference data '
-                    'from {} and put in "challenge" subdirectory'.format(url))
 
-modelParamsFile = r'./modelParams.txt'
-cases = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+if __name__ == '__main__':
 
-for case in cases:
-    if not os.path.isfile(r'./challenge/{}.mat'.format(str(case).zfill(2))):
-        url = r'"http://dl.dropbox.com/u/5131732/' + \
-            r'ISMRM_Challenge/data_matlab.zip"'
-        raise Exception(
-            r'Please download ISMRM 2012 challenge datasets from {} '
-            r'and put in "challenge" subdirectory'.format(url))
+    if not os.path.isfile(r'./challenge/refdata.mat'):
+        url = r'"https://dl.dropboxusercontent.com/u/5131732/' + \
+            r'ISMRM_Challenge/refdata.mat"'
+        raise Exception(r'Please download ISMRM 2012 challenge reference data '
+                        'from {} and put in "challenge" subdirectory'.format(url))
 
-results = []
-for case in cases:
-    dataParamsFile = r'./challenge/{}.txt'.format(case)
-    if case == 9:
-        algoParamsFile = r'./algoParams2D.txt'
-    else:
-        algoParamsFile = r'./algoParams3D.txt'
-    outDir = r'./challenge/{}_REC'.format(str(case).zfill(2))
-    t = time.time()
-    FWQPBO.FW(dataParamsFile, algoParamsFile, modelParamsFile, outDir)
-    results.append((case, getScore(case, outDir + '/ff'), time.time() - t))
+    modelParamsFile = r'./modelParams.txt'
+    cases = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-print()
-for case, score, recTime in results:
-    print('Case {}: score {}% in {} sec'.format(
-        case, round(score, 2), round(recTime, 1)))
+    for case in cases:
+        if not os.path.isfile(r'./challenge/{}.mat'.format(str(case).zfill(2))):
+            url = r'"http://dl.dropbox.com/u/5131732/' + \
+                r'ISMRM_Challenge/data_matlab.zip"'
+            raise Exception(
+                r'Please download ISMRM 2012 challenge datasets from {} '
+                r'and put in "challenge" subdirectory'.format(url))
+
+    results = []
+    for case in cases:
+        dataParamsFile = r'./challenge/{}.txt'.format(case)
+        if case == 9:
+            algoParamsFile = r'./algoParams2D.txt'
+        else:
+            algoParamsFile = r'./algoParams3D.txt'
+        outDir = r'./challenge/{}_REC'.format(str(case).zfill(2))
+        t = time.time()
+        FWQPBO.FW(dataParamsFile, algoParamsFile, modelParamsFile, outDir)
+        results.append((case, getScore(case, outDir + '/ff'), time.time() - t))
+
+    print()
+    for case, score, recTime in results:
+        print('Case {}: score {}% in {} sec'.format(
+            case, round(score, 2), round(recTime, 1)))
